9f7f32fdedbe0657f71a5196a59fcc431faaeef2
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-04-06 10:06:02 +0200

    Fix version to be mix-compliant

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exmath.Mixfile do
 
   def project do
     [app: :exmath,
-     version: "0.2.1.1",
+     version: "0.2.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
